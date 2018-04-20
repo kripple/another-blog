748dc2d93f3dc876f60cf7aa1a891d9c9ba46680
Kelly Ripple <kelly.m.ripple@gmail.com>
2018-04-20 18:34:13 -0400

    add draft deploy script

--- scripts/deploy-master-to-github-pages.sh
+++ scripts/deploy-master-to-github-pages.sh
@@ -2,13 +2,17 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo -t beautifulhugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# Choose master branch.
+git checkout master 
 
-# Go To Public folder
-cd public
+# Remove previous build.
+rm -rf public
+
+# Build the project.
+hugo -t beautifulhugo 
+
 # Add changes to git.
-git add .
+git add public
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -17,8 +21,14 @@ if [ $# -eq 1 ]
           fi
           git commit -m "$msg"
 
+# create a local gh-pages branch containing the splitted output folder
+git subtree split --prefix public -b gh-pages 
+
+# force the push of the gh-pages branch to the remote gh-pages branch at origin
+git push -f origin gh-pages:gh-pages 
+
+# delete the local gh-pages because you will need it: ref
+git branch -D gh-pages 
+
 # Push source and build repos.
 git push origin master
-
-# Come Back up to the Project Root
-cd ..
